9d4d779a5c8d309c0cde950f0b0917ab80543a0c
srinivassun <srinivasarao.sunnam29@gmail.com>
2026-02-08 00:13:02 +0530

    App 6 - Project Solution for Dictionary api app

---
+++ translate_api_project.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+import pandas as pd
+
+app = Flask(__name__)
+df = pd.read_csv("dictionary.csv")
+@app.route("/")
+def home():
+    return render_template("translate.html")
+
+@app.route("/api/v1/<word>")
+def api(word):
+
+    definition = df.loc[df['word'] == word]['definition'].squeeze()
+    return {"definition":definition,
+            "word":word}
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5002)
+
