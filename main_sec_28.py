844dd74d9fa8e0d10c1d4010f75344f6402174dd
srinivassun <srinivasarao.sunnam29@gmail.com>
2026-02-21 18:51:51 +0530

    Sec 28 passing data to the html

---
+++ main_sec_28.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template
+import pandas as pd
+
+app = Flask(__name__)
+
+stations = pd.read_csv("data_small/stations.txt",skiprows=17)
+
+@app.route("/")
+def home():
+    return render_template("home_1.html",data=stations.to_html())
+
+@app.route("/api/v1/<station>/<date>")
+def about(station, date):
+    file_name = "data_small/TG_STAID"+str(station).zfill(6)+".txt"
+    print(file_name)
+    print(station,date)
+    df = pd.read_csv(file_name,skiprows=20,parse_dates=["    DATE"])
+    temperature = df.loc[df['    DATE'] == date]['   TG'].squeeze() / 10
+
+    return {"station":station,
+            "date":date,
+            "temperature":temperature}
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
+
